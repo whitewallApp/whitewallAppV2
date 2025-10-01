b2f54bd70980ba80d79ead656319afe1a3629aad
Thomas Dick <thomas.ed.dick@gmail.com>
2025-10-01 14:59:46 -0600

    updated local ip

--- app/utils/api.ts
+++ app/utils/api.ts
@@ -1,6 +1,6 @@
-export const API_BASE = "http://192.168.87.30/requests/v1";
+export const API_BASE = "http://172.25.64.1/requests/v1";
 
-export const BASE_URL = "http://192.168.87.30"
+export const BASE_URL = "http://172.25.64.1"
 export const API_KEY = "0abb68a42da22fb9d5fe7ed7d1a918b81c9355920cafd707dde065749169f774";
 
 export async function fetchData(apiKey: string) {
